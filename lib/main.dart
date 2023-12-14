eaceb59e7e788713995507d13e22f8e095894986
rei <r.zerostage@gmail.com>
2023-12-14 15:57:31 +0900

    chenged app's title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Git T2est',
+      title: 'Keep Clean',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
